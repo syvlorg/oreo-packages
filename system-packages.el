92c58d98bc7282df9fd6f24436a105f5f518cde9
Alex Branham <alex.branham@gmail.com>
2020-04-26 13:14:12 -0400

    Update copyright

--- system-packages.el
+++ system-packages.el
@@ -1,6 +1,6 @@
 ;;; system-packages.el --- functions to manage system packages -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2016-2019 Free Software Foundation, Inc.
+;; Copyright (C) 2016-2020 Free Software Foundation, Inc.
 
 ;; Author: J. Alexander Branham <alex.branham@gmail.com>
 ;; Maintainer: J. Alexander Branham <alex.branham@gmail.com>
